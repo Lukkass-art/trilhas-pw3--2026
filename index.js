9bdf97dad9a1b7654a081baae5fe3f2ad9b6c85b
Lucas Silvestre <lucas1.silvestre@alunos.ifsuldeminas.edu.br>
2026-02-11 15:09:00 -0300

    Teste de atualização

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) =>{
-    res.send("Servidor Funcionando!");
+    res.send(" Servidor Funcionando!");
 });
  
 app.listen(3000,() =>{
